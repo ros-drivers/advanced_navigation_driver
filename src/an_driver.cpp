2604368dd68d2a09048fc83a2667a76cd81b0e80
Kyler Laird <kylerlaird@users.noreply.github.com>
2018-03-12 12:52:10 -0500

    oops
    I uploaded the wrong file last time.  See previous commit.

--- src/an_driver.cpp
+++ src/an_driver.cpp
@@ -26,12 +26,23 @@
  * DEALINGS IN THE SOFTWARE.
  */
 
+/*
+2018.02.09  Kyler Laird
+added RTCM handling, configuration parameters
+
+2018.03.10  Kyler Laird
+added REP compliance, transform
+*/
+
 #include <ros/ros.h>
+#include <ros/serialization.h>
+#include <tf/transform_broadcaster.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdint.h>
 #include <math.h>
 #include <unistd.h>
+#include <string.h>
 
 #include "rs232/rs232.h"
 #include "an_packet_protocol.h"
@@ -42,33 +53,158 @@
 #include <sensor_msgs/Imu.h>
 #include <geometry_msgs/Twist.h>
 #include <diagnostic_msgs/DiagnosticArray.h>
+#include <std_msgs/String.h>
 
 #define RADIANS_TO_DEGREES (180.0/M_PI)
 
+
+int an_packet_transmit(an_packet_t *an_packet)
+{
+        an_packet_encode(an_packet);
+	return SendBuf(an_packet_pointer(an_packet), an_packet_size(an_packet));
+}
+
+void set_filter_options()
+{
+        an_packet_t *an_packet;
+        filter_options_packet_t filter_options_packet;
+
+        /* initialise the structure by setting all the fields to zero */
+        memset(&filter_options_packet, 0, sizeof(filter_options_packet_t));
+
+        filter_options_packet.permanent = TRUE;
+        filter_options_packet.vehicle_type = vehicle_type_car;
+        filter_options_packet.internal_gnss_enabled = TRUE;
+        filter_options_packet.atmospheric_altitude_enabled = TRUE;
+        filter_options_packet.velocity_heading_enabled = TRUE;
+        filter_options_packet.reversing_detection_enabled = TRUE;
+        filter_options_packet.motion_analysis_enabled = TRUE;
+
+        an_packet = encode_filter_options_packet(&filter_options_packet);
+
+        an_packet_transmit(an_packet);
+
+        an_packet_free(&an_packet);
+}
+
+void set_filter_options_x()
+{
+	//an_packet_t *an_packet = an_packet_allocate(17, 186);
+
+	an_packet_t *an_packet = an_packet_allocate(4, 55);
+	memcpy(&an_packet->data[0], "test", 5 * sizeof(uint8_t));
+        an_packet_transmit(an_packet);
+        an_packet_free(&an_packet);
+}
+
+void handle_rtcm(const std_msgs::String::ConstPtr& msg) {
+	const char *rtcm_data;
+	uint32_t string_length = msg->data.length();
+
+	// ROS_INFO("RTCM: %d bytes",  string_length);
+	
+	an_packet_t *an_packet = an_packet_allocate(string_length, packet_id_rtcm_corrections);
+	memcpy(&an_packet->data[0], &msg->data[0], string_length);
+	an_packet_transmit(an_packet);
+	an_packet_free(&an_packet);
+}
+
+
+// LatLong-UTM.c++
+// Conversions:  LatLong to UTM;  and UTM to LatLong;
+// by Eugene Reimer, ereimer@shaw.ca, 2002-December;
+// with LLtoUTM & UTMtoLL routines based on those by Chuck Gantz chuck.gantz@globalstar.com;
+// with ellipsoid & datum constants from Peter H Dana website (http://www.colorado.edu/geography/gcraft/notes/datum/edlist.html);
+//
+// Usage:  see the Usage() routine below;
+//
+// Copyright © 1995,2002,2010 Eugene Reimer, Peter Dana, Chuck Gantz.  Released under the GPL;  see http://www.gnu.org/licenses/gpl.html
+// (Peter Dana's non-commercial clause precludes using the LGPL)
+
+
+#include <cmath>			//2010-08-11: was <math.h>	  
+#include <cstdio>			//2010-08-11: was <stdio.h>	  
+
+#define fr 298.257223563
+#define a 6378137
+#define k0 0.9996
+const double PI       =	4*atan(1);	//Gantz used: PI=3.14159265;
+const double deg2rad  = PI/180;
+const double ee = 2/fr-1/(fr*fr);
+const double EE = ee/(1-ee);
+double LongOriginRad;
+
+void LLtoUTM(int Zone, double LatRad, double LongRad,  double& Northing, double& Easting) {
+	// converts LatLong to UTM coords;  3/22/95: by ChuckGantz chuck.gantz@globalstar.com, from USGS Bulletin 1532.
+	double N, T, C, A, M;
+
+	N = a/sqrt(1-ee*sin(LatRad)*sin(LatRad));
+	T = tan(LatRad)*tan(LatRad);
+	C = EE*cos(LatRad)*cos(LatRad);
+	A = cos(LatRad)*(LongRad-LongOriginRad);
+
+	M= a*((1 - ee/4    - 3*ee*ee/64 - 5*ee*ee*ee/256  ) *LatRad 
+	    - (3*ee/8 + 3*ee*ee/32 + 45*ee*ee*ee/1024) *sin(2*LatRad)
+	    + (15*ee*ee/256 + 45*ee*ee*ee/1024	  ) *sin(4*LatRad)
+	    - (35*ee*ee*ee/3072			  ) *sin(6*LatRad));
+
+	Easting = k0*N*(A+(1-T+C)*A*A*A/6+(5-18*T+T*T+72*C-58*EE)*A*A*A*A*A/120) + 500000.0;
+
+	Northing = k0*(M+N*tan(LatRad)*(A*A/2+(5-T+9*C+4*C*C)*A*A*A*A/24
+			    + (61-58*T+T*T+600*C-330*EE)*A*A*A*A*A*A/720));
+}
+
+
+
+
+
+
 int main(int argc, char *argv[]) {
 	// Set up ROS node //
-	ros::init(argc, argv, "an_device_node");
-	ros::NodeHandle nh;
-	
-	if(argc != 3)
+	ros::init(argc, argv, "an_device");
+	ros::NodeHandle nh("~");
+
+	// Set up the COM port
+	std::string com_port_s;
+	nh.param<std::string>("port", com_port_s, "/dev/ttyS0");
+	char *com_port = (char *)com_port_s.c_str();
+
+	int baud_rate;
+	nh.param<int>("baud", baud_rate, 115200);
+
+	if (OpenComport(com_port, baud_rate))
 	{
-		printf("\nCannot start - not enough commnand line arguments. \nUsage: rosrun an_driver an_driver {port} {baud rate}. \nTry: rosrun an_driver an_driver /dev/ttyUSB0 115200\n");
-		printf("Number of command line arguments detected: %i\n",argc);
+		ROS_INFO("Could not open serial port %s at %d baud.", com_port, baud_rate);
 		exit(EXIT_FAILURE);
 	}
-	
-	printf("\nYour Advanced Navigation ROS driver is currently running\nClose the Terminal window when done.\n");
-	
-	// Set up the COM port
-	char* com_port = argv[1];
-	int baud_rate = atoi(argv[2]);
+	ROS_INFO("port:%s@%d", com_port, baud_rate);
+
+	// If an RTCM topic is provided, subscribe to it and pass corrections to device.
+	std::string rtcm_topic;
+	ros::Subscriber rtcm_sub;
+	if (nh.getParam("rtcm", rtcm_topic)) {
+		rtcm_sub = nh.subscribe(rtcm_topic.c_str(), 1000, handle_rtcm);
+		ROS_INFO("listening for RTCM on %s", rtcm_topic.c_str());
+	}
+
+	// If a UTM Zone is provided, publish transforms.
+	// The zone is static to avoid problems due to changing when near a Zone boundary.
+	int utm_zone;
+	std::string tf_name;
+	tf::Transform transform;
+	if (nh.getParam("utm_zone", utm_zone)) {
+		LongOriginRad = (utm_zone*6 - 183) * deg2rad;
+		nh.param<std::string>("tf_name", tf_name, "an_device");  // The default should be chosen better.
+		ROS_INFO("using UTM Zone %d to publish transform %s", utm_zone, tf_name.c_str());
+
+	}
 
 	// Initialise Publishers and Topics //
-	ros::Publisher nav_sat_fix_pub=nh.advertise<sensor_msgs::NavSatFix>("an_device/NavSatFix",10);
-	ros::Publisher twist_pub=nh.advertise<geometry_msgs::Twist>("an_device/Twist",10);
-	ros::Publisher imu_pub=nh.advertise<sensor_msgs::Imu>("an_device/Imu",10);
-	ros::Publisher system_status_pub=nh.advertise<diagnostic_msgs::DiagnosticStatus>("an_device/SystemStatus",10);
-	ros::Publisher filter_status_pub=nh.advertise<diagnostic_msgs::DiagnosticStatus>("an_device/FilterStatus",10);
+	ros::Publisher nav_sat_fix_pub=nh.advertise<sensor_msgs::NavSatFix>("NavSatFix",10);
+	ros::Publisher twist_pub=nh.advertise<geometry_msgs::Twist>("Twist",10);
+	ros::Publisher imu_pub=nh.advertise<sensor_msgs::Imu>("Imu",10);
+	ros::Publisher system_status_pub=nh.advertise<diagnostic_msgs::DiagnosticStatus>("SystemStatus",10);
+	ros::Publisher filter_status_pub=nh.advertise<diagnostic_msgs::DiagnosticStatus>("FilterStatus",10);
 
 	// Initialise messages
 	sensor_msgs::NavSatFix nav_sat_fix_msg;
@@ -126,16 +262,12 @@ int main(int argc, char *argv[]) {
 	quaternion_orientation_standard_deviation_packet_t quaternion_orientation_standard_deviation_packet;
 	int bytes_received;
 	
-	if (OpenComport(com_port, baud_rate))
-	{
-		printf("Could not open serial port: %s \n",com_port);
-		exit(EXIT_FAILURE);
-	}
 
 	an_decoder_initialise(&an_decoder);
 
+
 	// Loop continuously, polling for packets
-	while (1)
+	while (ros::ok())
 	{
 		if ((bytes_received = PollComport(an_decoder_pointer(&an_decoder), an_decoder_size(&an_decoder))) > 0)
 		{
@@ -145,6 +277,18 @@ int main(int argc, char *argv[]) {
 			// decode all the packets in the buffer //
 			while ((an_packet = an_packet_decode(&an_decoder)) != NULL)
 			{
+				// acknowledgement packet //
+				if (an_packet->id == 0) 
+				{
+					ROS_INFO("acknowledgement data: %d", an_packet->data[3]);
+				}
+
+				// receiver information packet //
+				if (an_packet->id == 69) 
+				{
+					ROS_INFO("receiver information: %d", an_packet->data[0]);
+				}
+
 				// system state packet //
 				if (an_packet->id == packet_id_system_state) 
 				{
@@ -153,21 +297,21 @@ int main(int argc, char *argv[]) {
 						// NavSatFix
 						nav_sat_fix_msg.header.stamp.sec=system_state_packet.unix_time_seconds;
 						nav_sat_fix_msg.header.stamp.nsec=system_state_packet.microseconds*1000;
-						if ((system_state_packet.filter_status.b.gnss_fix_type == 1) ||
-							(system_state_packet.filter_status.b.gnss_fix_type == 2))
+						if ((system_state_packet.filter_status.b.gnss_fix_type == 1) ||  // 2D
+							(system_state_packet.filter_status.b.gnss_fix_type == 2))   // 3D
 						{
-							nav_sat_fix_msg.status.status=0;
+							nav_sat_fix_msg.status.status=0; // no fix
 						}
-						else if ((system_state_packet.filter_status.b.gnss_fix_type == 3) ||
-							 (system_state_packet.filter_status.b.gnss_fix_type == 5))
+						else if ((system_state_packet.filter_status.b.gnss_fix_type == 3) ||  // SBAS
+							 (system_state_packet.filter_status.b.gnss_fix_type == 5))   // Omnistar/Starfire
 						{
-							nav_sat_fix_msg.status.status=1;
+							nav_sat_fix_msg.status.status=1; // SBAS
 						}
-						else if ((system_state_packet.filter_status.b.gnss_fix_type == 4) ||
-							 (system_state_packet.filter_status.b.gnss_fix_type == 6) ||
-							 (system_state_packet.filter_status.b.gnss_fix_type == 7))
+						else if ((system_state_packet.filter_status.b.gnss_fix_type == 4) ||  // differential
+							 (system_state_packet.filter_status.b.gnss_fix_type == 6) ||  // RTK float
+							 (system_state_packet.filter_status.b.gnss_fix_type == 7))    // RTK fixed
 						{
-							nav_sat_fix_msg.status.status=2;
+							nav_sat_fix_msg.status.status=2; // GBAS
 						}
 						else 
 						{
@@ -191,21 +335,19 @@ int main(int argc, char *argv[]) {
 						// IMU
 						imu_msg.header.stamp.sec=system_state_packet.unix_time_seconds;
 						imu_msg.header.stamp.nsec=system_state_packet.microseconds*1000;
+
 						// Convert roll, pitch, yaw from radians to quaternion format //
-						float phi = system_state_packet.orientation[0] / 2.0f;
-						float theta = system_state_packet.orientation[1] / 2.0f;
-						float psi = system_state_packet.orientation[2] / 2.0f;
-						float sin_phi = sinf(phi);
-						float cos_phi = cosf(phi);
-						float sin_theta = sinf(theta);
-						float cos_theta = cosf(theta);
-						float sin_psi = sinf(psi);
-						float cos_psi = cosf(psi);				
-						imu_msg.orientation.x=-cos_phi * sin_theta * sin_psi + sin_phi * cos_theta * cos_psi;
-						imu_msg.orientation.y=cos_phi * sin_theta * cos_psi + sin_phi * cos_theta * sin_psi;
-						imu_msg.orientation.z=cos_phi * cos_theta * sin_psi - sin_phi * sin_theta * cos_psi;
-						imu_msg.orientation.w=cos_phi * cos_theta * cos_psi + sin_phi * sin_theta * sin_psi;
-						
+						tf::Quaternion orientation;
+						orientation.setRPY(
+							system_state_packet.orientation[0],
+							system_state_packet.orientation[1],
+							PI / 2.0f - system_state_packet.orientation[2] // REP 103
+						);
+						imu_msg.orientation.x = orientation[0];
+						imu_msg.orientation.y = orientation[1];
+						imu_msg.orientation.z = orientation[2];
+						imu_msg.orientation.w = orientation[3];
+
 						imu_msg.angular_velocity.x=system_state_packet.angular_velocity[0]; // These the same as the TWIST msg values
 						imu_msg.angular_velocity.y=system_state_packet.angular_velocity[1];
 						imu_msg.angular_velocity.z=system_state_packet.angular_velocity[2];
@@ -213,6 +355,39 @@ int main(int argc, char *argv[]) {
 						imu_msg.linear_acceleration.y=system_state_packet.body_acceleration[1];
 						imu_msg.linear_acceleration.z=system_state_packet.body_acceleration[2];
 						
+						// transform
+						if (utm_zone) {
+							static tf::TransformBroadcaster transform_br;
+							double N, E;
+
+							LLtoUTM(
+								utm_zone,
+								system_state_packet.latitude,
+								system_state_packet.longitude,
+								N, E
+							);
+
+							transform.setOrigin(tf::Vector3(
+								E, N, system_state_packet.height
+								// 0, 0, 0  // move to origin for testing
+								// E - 483713.0, N - 4526330.0, 0  // move to origin for testing
+							));
+
+							transform.setRotation(orientation);
+
+							transform_br.sendTransform(tf::StampedTransform(
+								transform,
+								//ros::Time::now(),
+								ros::Time(
+									system_state_packet.unix_time_seconds,
+									system_state_packet.microseconds*1000
+								),
+								"world",  // Is it reasonable to hardcode this?
+								tf_name
+							));
+							//printf("time: %d\n", system_state_packet.microseconds);
+						}	
+
 						// System Status	
 						system_status_msg.message = "";
 						system_status_msg.level = 0; // default OK state
@@ -391,7 +566,7 @@ int main(int argc, char *argv[]) {
 						imu_msg.orientation_covariance[8] = quaternion_orientation_standard_deviation_packet.standard_deviation[2];						
 					}
 				}
-				// Ensure that you free the an_packet when your done with it //
+				// Ensure that you free the an_packet when you're done with it //
 				// or you will leak memory                                   //
 				an_packet_free(&an_packet);
 
@@ -403,12 +578,7 @@ int main(int argc, char *argv[]) {
 				filter_status_pub.publish(filter_status_msg);
 			}
 		}
+		ros::spinOnce();
 	}
 
 }
-
-
-
-	
-	
-
